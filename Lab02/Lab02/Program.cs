529f64bb4a403ee193a2cc6d16c9efb5ba118f79
Connor Hagerman <S01217651@acad.tri-c.edu>
2020-11-01 11:47:38 -0500

    finshed step 2

--- Lab02/Lab02/Program.cs
+++ Lab02/Lab02/Program.cs
@@ -10,7 +10,21 @@ namespace Lab02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            int number1; // declare first number to multiply
+            int number2; // declare first number to multiply 
+            int sum; // declare sum of number 1 and number 2 
+
+            Console.Write("Enter first interger: "); // prompt user 
+            //read.first number from user 
+            number1 = Convert.ToInt32(Console.ReadLine() );
+
+            Console.Write("Enter second interger: "); // prompt user 
+            // read second number from user
+            number2 = Convert.ToInt32(Console.ReadLine() );
+
+            sum = number1 + number2; // multiply numbers 
+
+            Console.WriteLine("Sum is (0)", sum); // display sum
         }
     }
 }

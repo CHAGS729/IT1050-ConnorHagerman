c15907cc3080cc0f1cfb366d234ae7de88b6766e
Connor Hagerman <S01217651@acad.tri-c.edu>
2020-11-09 22:41:25 -0500

    Completed Lab 3

--- Lab03/Lab03/Program.cs
+++ Lab03/Lab03/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Configuration;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,23 @@ namespace Lab03
     {
         static void Main(string[] args)
         {
-            
+
+            /*
+             * For Loop: https://www.youtube.com/watch?v=lvbVZ1C1rrk
+             * If statement: https://www.youtube.com/watch?v=z_B7oVDs-tA
+             * While Loop: https://www.youtube.com/watch?v=7JX2XH4zeNM
+             */
+
+            int intPi = 22 / 7;
+            float floatPi = 22f / 7f;
+            double doublePi = 22d / 7d;
+            decimal decimalPi = 22m / 7m;
+
+            Console.WriteLine($"int: {intPi}");
+            Console.WriteLine($"float: {floatPi}");
+            Console.WriteLine($"double: {doublePi}");
+            Console.WriteLine($"decimal: {decimalPi}");
+
             int speedLimit = 35;
             int speed = 35;
 
@@ -26,9 +43,35 @@ namespace Lab03
             {
                 Console.WriteLine("You're going a little fast, False");
             }
-           
+
+            /*
+             * 	1.	if statement - Write a program that converts a Fahrenheit temperature to Celsius.  The user should be able to input the temperature at the Command prompt and it should output the temperature.  In addition, you should output “It is cold” if the Fahrenheit value is less that 40 or it should output “It is hot” if the temperature is over 90. 
+             */
+            double fahrenheit;
+            Console.Write("Enter a temperature: ");
+            fahrenheit = Convert.ToDouble(Console.ReadLine());
+            double celsius = (fahrenheit - 32d) * 5d / 9d;
+
+            if (fahrenheit >= celsius)
+            {
+                Console.WriteLine("It is hot"); 
+            }
+            if (celsius == fahrenheit)   
+            {
+                Console.WriteLine("It is cold");
+            }
+
+            Console.WriteLine("Program is done");
+
+
+            int i = 1;
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i++; // i = i + i
+            }
+
+            Console.WriteLine("Program complete");
         }
     }
 }
-
-

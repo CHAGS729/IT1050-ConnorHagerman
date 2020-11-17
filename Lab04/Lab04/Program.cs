d4096a3b8d1ea0f15d90ebebf94322922d88c138
Connor Hagerman <S01217651@acad.tri-c.edu>
2020-11-16 23:09:05 -0500

    Kinda completed lab 4

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -25,12 +25,74 @@ namespace Lab04
              * 
              */
 
-            for (int i = 1; i < 3; i++)
+
+           
+             int i = 1; 
+             while (i < 99)
+             {
+                 Console.WriteLine(i);
+                 i++; // i = i + 1
+             }
+            
+               for ( ; i < 3; i++)
+           {
+               Console.WriteLine(i); 
+           }
+
+               if ((i % 2) == 0)
             {
-                Console.WriteLine(i); 
+                Console.WriteLine("Integer is Even" + i);
+            }
+               else
+            {
+                Console.WriteLine("Integer is Odd" + i);
             }
 
-            Console.WriteLine("Program complete");
+           Console.WriteLine("Program complete");
+
+
+            int temp = 1;
+
+
+
+          if (temp < 10)
+            {
+                Console.WriteLine("Print Polar Bears");
+            }
+            else if (temp < 20)
+            {
+                Console.WriteLine("Print Penguin");
+            }
+           if (temp < 40)
+            {
+                Console.WriteLine("Print Moose");
+            }
+           if (temp < 50)
+            {
+                Console.WriteLine("Print Reindeer");
+            }
+           if (temp < 60)
+            {
+                Console.WriteLine("Print Deer");
+            }
+           if (temp < 70)
+            {
+                Console.WriteLine("Print Turtle"); 
+            }
+           if (temp < 80)
+            {
+                Console.WriteLine("Print Lion"); 
+            }
+           if (temp < 90)
+            {
+                Console.WriteLine("Print Fish");
+            }
+            else
+            {
+                Console.WriteLine("Print Bug");
+            }
+
+            Console.ReadKey();
         }
     }
 }

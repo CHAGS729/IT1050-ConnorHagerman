bdd81d3a3ebcbbc6d98a57fcade53ee675d7c6e9
Connor Hagerman <S01217651@acad.tri-c.edu>
2020-12-07 15:40:51 -0500

    almost done

---
+++ Lab005/Lab005/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(getArea(2, 3));
+            Console.ReadLine();
+        }
+        public static double getArea(double height, double width)
+        {
+            double area;
+            area = height * width;
+            return area;
+
+            {
+                Console.WriteLine("{0} is Barking", name);
+            }
+
+
+     public string bark;
+        private static void
+            
+
+            {
+                Console.WriteLine("{0} is so smart! {0} is doing a {1}", name, trickName);
+            }
+    
+
+    }
+}
+}
+ /* Public defines the scope     
+  * double is the return type      
+  * getArea is the name of the method      
+  * (double height, and double width are the peraminters)      
+  * the method body is all the open and closed curly brakets       
+  * double area;, area = height * width, return area;       
+  */  
+ /* A static method uses comile time binding or early binding.    
+  * Non static method uses run time binding or dynamic binding.    
+  * A static method cannot be overridden being compile time binding.   
+  * A non static method can be onverride being dynamic binding.       
+  */
+

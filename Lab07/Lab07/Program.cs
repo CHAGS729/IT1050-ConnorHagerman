714f55b564c7ad50a349c34f276cacf848d61b03
Connor Hagerman <S01217651@acad.tri-c.edu>
2020-12-10 18:39:24 -0500

    Completed the lab 7

---
+++ Lab07/Lab07/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book myBook = new Book();
+            myBook.Title = "Beginners to coding";
+            myBook.Author = "Connor Hagerman";
+            myBook.Year = 2000;
+            Console.WriteLine("{0} {1} {2}", myBook.Title, myBook.Author, myBook.Year);
+            Console.ReadLine();
+
+
+            Book myBookTest;
+            myBookTest = myBook;
+
+            Console.WriteLine("{0} {1} {2}", myBook.Title, myBook.Author, myBook.Year);
+            Console.ReadLine();
+
+            myBookTest.Year = 2000;
+        }
+    }
+
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }               
+
+
+        public Book()
+        {
+            Title = "Beginner to coding"; 
+        }
+    }    
+}
